f45cae088780445f34d37f93dbad02ab67e36891
Julianxalessio <julian.lombardo@outlook.com>
2024-12-26 13:34:58 +0100

    Passwortschutz im Timer.html hizugefügt und komplett vervollständigt

--- JS Code/Timer.js
+++ JS Code/Timer.js
@@ -1,6 +1,9 @@
 let Minutes = 14;
 let Seconds = 10;
-
+let login = localStorage.getItem('Login');
+if (login === "false"){
+    window.location.replace("https://www.google.com/");
+}
 function startCountdown() {
     function countdown() {
         if (Seconds === 0 && Minutes === 0) {
@@ -13,6 +16,10 @@ function startCountdown() {
             Seconds = 59;
         } else {
             Seconds--;
+        }
+        if(Minutes === 0 & Seconds === 0){
+            localStorage.setItem('Login', "false");
+            window.location.replace("https://www.google.com/");
         }
 
         Countdown.textContent = `${fuehrendeNull(Minutes)}:${fuehrendeNull(Seconds)}`;
@@ -29,3 +36,38 @@ function fuehrendeNull(zahl) {
 }
 
 startCountdown();
+
+function markInternalNavigation() {
+    sessionStorage.setItem('isInternalNavigation', "true");
+}
+
+document.querySelectorAll('a').forEach(function (link) {
+    link.addEventListener('click', function (e) {
+        if (link.hostname === window.location.hostname) {
+            markInternalNavigation(); 
+        }
+    });
+});
+
+window.addEventListener('beforeunload', function () {
+    if (sessionStorage.getItem('isInternalNavigation') === "false") {
+        localStorage.setItem('Login', "false");
+    }
+});
+
+sessionStorage.setItem('isInternalNavigation', "false");
+
+
+function refreshTimer() {
+    Minutes = 15;
+    Seconds = 0;
+    console.log('Timer zurückgesetzt!');
+}
+
+document.addEventListener('mousemove', refreshTimer);
+document.addEventListener('keydown', refreshTimer);
+document.addEventListener('click', refreshTimer);
+
+document.addEventListener('touchstart', refreshTimer);
+document.addEventListener('touchmove', refreshTimer);
+document.addEventListener('touchend', refreshTimer);
